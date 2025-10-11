5c6390381fd4124baf60b674da67d934bd9af700
Kartyos-India <128411905+Kartyos-India@users.noreply.github.com>
2025-10-11 13:27:32 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,10 @@
+{
+  "name": "ssb-psychology-platform",
+  "version": "1.0.0",
+  "description": "Consolidated SSB Psychology Platform.",
+  "main": "script.js",
+  "type": "module",
+  "scripts": {
+    "start": "node script.js"
+  }
+}
