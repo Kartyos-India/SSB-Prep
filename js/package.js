1d9dd1b6a09f78a3fd8987601d98390e72122b01
Kartyos-India <128411905+Kartyos-India@users.noreply.github.com>
2025-10-12 19:10:23 +0530

    Create package.js

---
+++ js/package.js
@@ -0,0 +1,11 @@
+{
+  "name": "ssb-prep-platform",
+  "version": "1.0.0",
+  "description": "SSB Preparation Platform",
+  "main": "index.js",
+  "type": "module",
+  "dependencies": {
+    "buffer": "^6.0.3"
+  }
+}
+
